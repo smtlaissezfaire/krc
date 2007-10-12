2237322e6c237493420203bc8f04e7aaf20d01b1
smt <smt@693cc886-b2ff-4f4c-8249-cf8ba0c123bd>
2007-10-12 05:34:33 +0000

    Chapter 01, sections 04 and 05
    git-svn-id: https://thmadb.com/svn/krc@4 693cc886-b2ff-4f4c-8249-cf8ba0c123bd

---
+++ ch01-04/fahr_to_cels_with_constants.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define LOWER 	0
+#define UPPER 	100
+#define STEP 		20
+
+main() {
+	int fahr;
+	
+	for(fahr = LOWER; fahr <= UPPER; fahr += STEP)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
